c85b529c56dbe25882bee96b7c08ad7025bcf0f7
freddwithy <134991353+freddwithy@users.noreply.github.com>
2024-03-26 12:20:44 -0400

    create page

---
+++ app/create/page.tsx
@@ -0,0 +1,11 @@
+import Container from "@/app/components/ui/Container"
+
+export default function CreatePage () {
+    return (
+        <Container>
+            <div>
+                Hola mundo
+            </div>
+        </Container>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Hero } from "./components/Hero";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex flex-col items-center justify-between pt-24">
        <Hero />
     </main>
   );
