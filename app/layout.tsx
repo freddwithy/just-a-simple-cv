c2d06c7d7a89686aa963ceff53a1368b6d822ca8
freddwithy <134991353+freddwithy@users.noreply.github.com>
2024-04-05 21:39:29 -0400

    creating toast provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
+import { ToasterProvider } from "@/providers/toast-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body className={`${inter.className}`}>
+        <ToasterProvider />
         <Header />
         {children}
         <Footer />
