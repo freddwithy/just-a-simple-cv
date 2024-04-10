55ad4d0843761b1a3c43a75bdea2fda7eac7e8e5
freddwithy <134991353+freddwithy@users.noreply.github.com>
2024-04-10 11:33:16 -0400

    fixing key map problem

--- app/components/Features.tsx
+++ app/components/Features.tsx
@@ -15,8 +15,8 @@ const Features = () => {
         <Container className="w-full max-w-xl flex justify-center ">
             <div className="grid grid-cols-3 grid-rows-2 w-full justify-center items-center gap-2">
                 {
-                    features.map((feat) => (
-                        <span className="flex gap-2 w-full font-semibold text-start">
+                    features.map((feat, i) => (
+                        <span key={i} className="flex gap-2 w-full font-semibold text-start">
                             <Check className="text-orange-700" />
                             {feat}
                         </span>
