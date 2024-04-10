7390a5dad724058ebf19b7729e5f89ab7d3ec855
freddwithy <134991353+freddwithy@users.noreply.github.com>
2024-04-10 14:53:38 -0400

    styling button components and adding logo

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,6 +1,6 @@
-import Link from "next/link"
 import Container from "./ui/Container"
 import LogoLink from "./ui/LogoLink"
+import ActionButton from "./ui/ActionButton"
 
 export const Hero = () => {
     return (
@@ -10,9 +10,7 @@ export const Hero = () => {
                 <div className="flex justify-center flex-col gap-y-4 items-center">
                     <h1 className="text-6xl font-bold text-balance text-black">Create your CV faster than ever</h1>
                     <p className="text-pretty text-xl text-mystic-700"><span className="text-orange-500 font-medium">Stop wasting time</span> with complitaced templates, make your CV the <span className="text-orange-700 font-medium">faster and easies</span> way possible for free.</p>
-                    <Link href="/create" className="bg-mystic-700 px-6 py-4 rounded-lg w-auto hover:bg-mystic-600 transition-colors">
-                        <p className="font-semibold text-white">Create your Resume - It's Free!</p>
-                    </Link>
+                    <ActionButton link="/create" title="Create your Resume - It's Free!" />
                 </div>
             </div>
         </Container>
--- app/components/ui/LogoLink.tsx
+++ app/components/ui/LogoLink.tsx
@@ -5,8 +5,8 @@ import React from 'react'
 const LogoLink = () => {
   return (
     <Link href="/" className="flex gap-x-2 items-center justify-center">
-        <Image src="/logo.png" width={40} height={40} alt="justaresume logo"/>
-        <span className="font-bold text-2xl">Justaresume</span>
+        <Image src="/logo-green.png" width={40} height={40} alt="justaresume logo"/>
+        <span className="font-bold text-2xl ">Justaresume</span>
     </Link>
   )
 }
