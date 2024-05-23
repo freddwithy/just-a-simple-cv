4be6fbaefbb1432731960844a1818b82361da7f1
freddwithy <134991353+freddwithy@users.noreply.github.com>
2024-05-22 20:23:55 -0400

    trying responsive

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -8,7 +8,7 @@ export const Hero = () => {
             <LogoLink />
             <div className="flex justify-center items-center flex-col gap-y-4 text-center mt-10">
                 <div className="flex justify-center flex-col gap-y-4 items-center">
-                    <h1 className="text-6xl font-bold text-balance text-black">Craft Your CV Faster Than Ever</h1>
+                    <h1 className="text-2xl md:text-6xl font-bold text-balance text-black">Craft Your CV Faster Than Ever</h1>
                     <p className="text-pretty text-xl text-gray-600"><span className="text-orange-500 font-semibold">Stop wasting time</span> with complicated templates, make your resume the fastest and easiest way possible <span className="text-mystic-700 font-semibold">for free.</span></p>
                     <ActionButton link="/create" title="Create your Resume - It's Free!" />
                 </div>
